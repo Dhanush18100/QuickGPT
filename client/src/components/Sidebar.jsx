c467c0f54a5ed45eace7251b35c89636636a3ec7
Dhanush18100 <dhanush.nayak.100@gmail.com>
2025-10-25 13:56:40 +0530

    chatsection

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -13,7 +13,7 @@ const Sidebar = ({isMenuOpen,setIsMenuOpen}) => {
 
       {/* New chat button */}
       <button className='flex justify-center items-center w-full py-2 mt-10 text-white bg-gradient-to-r from-[#A456F7] to-[#3D81F6] text-sm rounded-md cursor-pointer'>
-        <spna className='mr-2 text-xl'>+</spna> New Chat
+        <span className='mr-2 text-xl'>+</span> New Chat
       </button>
 
       {/* Search conversation */}
