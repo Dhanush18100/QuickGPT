884e20c1251e9c3f3875a2ed47f6a855c540eea6
Dhanush18100 <dhanush.nayak.100@gmail.com>
2025-10-25 16:42:13 +0530

    community page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,17 @@
 import React, { useState } from 'react'
 import Sidebar from './components/Sidebar'
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route, useLocation} from 'react-router-dom'
 import Chatbox from './components/Chatbox'
 import Credits from './pages/Credits'
 import Community from './pages/Community'
 import { assets } from './assets/assets'
 import './assets/prism.css'
+import Loading from './pages/Loading'
 const App = () => {
   const [isMenuOpen ,setIsMenuOpen]=useState(false)
+  const {pathname}=useLocation()
+
+  if(pathname==='/loading') return <Loading/>
   return (
     <>
     {!isMenuOpen && <img src={assets.menu_icon} className='absolute top-3 left-3 w-8 h-8 cursor-pointer md:hidden not-dark:invert' onClick={()=>setIsMenuOpen(true)}/>}
